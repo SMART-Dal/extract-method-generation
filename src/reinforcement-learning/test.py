f3d4b18d5f04744a94c33c782f7ffc0fa1a5fd15
Indranil <indranil.palit@dal.ca>
2024-03-20 13:13:15 -0700

    added initial test script

--- src/reinforcement-learning/test.py
+++ src/reinforcement-learning/test.py
@@ -1,30 +1,70 @@
-import argparse
-import multiprocessing
-import os
-from utils import get_filenames, read_refactoring_examples, convert_examples_to_features, load_data
-from torch.utils.data import DataLoader, RandomSampler
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from datasets import load_dataset
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainingArguments, Seq2SeqTrainer, DataCollatorForSeq2Seq
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("--train_filename", default=None, type=str,
-                    help="The train filename. Should contain the .jsonl files for this task.")
-parser.add_argument("--dev_filename", default=None, type=str,
-                    help="The dev filename. Should contain the .jsonl files for this task.")
-parser.add_argument("--test_filename", default=None, type=str,
-                    help="The test filename. Should contain the .jsonl files for this task.")
+# Load datasets
+train_dataset = load_dataset("/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context", 
+                             split="train")
+test_dataset = load_dataset("/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context", 
+                            split="test")
+validation_dataset = load_dataset("/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context",
+                                 split="validation")
 
-args = parser.parse_args()
+print(len(train_dataset[0]["Smelly Sample"]))
+print(len(test_dataset))
+print(len(validation_dataset))
 
-print(args.dev_filename)
 
-tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-small")
 model = AutoModelForSeq2SeqLM.from_pretrained("Salesforce/codet5-small")
+tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-small")
 
-# pool = multiprocessing.Pool(os.cpu_count())
-args.train_filename, args.dev_filename, args.test_filename = get_filenames("src/refactoring-fine-tuning/data", "refactoring", None, None, "no-context")
-print(args.dev_filename)
-dev_examples, dev_data = load_data(args, args.dev_filename, tokenizer, 'dev')
-dev_sampler = RandomSampler(dev_data)
-dev_dataloader = DataLoader(dev_data, sampler=dev_sampler, batch_size=32)
+print(tokenizer.eos_token_id)
 
+def data_preprocessing(examples):
+    # example["Smelly Sample"] = example["Smelly Sample"].replace('\n', ' ')
+    # example["Method after Refactoring"] = example["Method after Refactoring"].replace('\n', ' ')
+    # example["Extracted Method"] = example["Extracted Method"].replace('\n', ' ')
+    # print(examples)
+    inputs = [example["Smelly Sample"].replace('\n', ' ') for example in examples]
+    targets = [example["Extracted Method"].replace('\n', ' ') + " " + example["Method after Refactoring"].replace('\n', ' ') for example in examples]
+    model_inputs = tokenizer(inputs, text_pair_target=targets, padding='max_length', truncation=True, max_length=512)
+    return model_inputs
+
+train_dataset = train_dataset.map(data_preprocessing, batched=True)
+validation_dataset = validation_dataset.map(data_preprocessing, batched=True)
+
+# def data_collator(batch):
+#     input_ids = tokenizer(batch["Smelly Sample"], padding='max_length', truncation=True, max_length=512).input_ids
+#     targets = tokenizer(batch["Method after Refactoring"]+batch["Extracted Method"],padding="max_length", truncation=True, return_tensors="pt").input_ids
+
+#     return {
+#         input_ids: input_ids,
+#         targets: targets
+#     }
+
+data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model="Salesforce/codet5-small")
+
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="./results",
+    num_train_epochs=3,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
+    evaluation_strategy="steps",
+    logging_strategy="steps",
+    logging_steps=500,
+    eval_steps=500,
+    save_steps=300
+)
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset,
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    # compute_metrics=compute_metrics,
+)
+
+trainer.train()
