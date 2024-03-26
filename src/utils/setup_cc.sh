f9db62c8fd01cdeb3f859930ed78926b7a9577f0
Indranil <indranil.palit@dal.ca>
2024-03-25 23:01:06 -0700

    added JAVA setup to script

--- src/utils/setup_cc.sh
+++ src/utils/setup_cc.sh
@@ -6,4 +6,10 @@ module load StdEnv/2023
 
 module load arrow/14.0.1
 
+module load java/17
+module load maven/3.9.6
+
+# export JAVA_TOOL_OPTIONS="-Xms256m -Xmx5g"
+unset JAVA_TOOL_OPTIONS
+
 # python -m venv .venv
