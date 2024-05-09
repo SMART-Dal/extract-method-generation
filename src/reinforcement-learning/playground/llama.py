d9287e9e499df20fc76f0a60d3690db1e7515888
Indranil <indranil.palit@dal.ca>
2024-05-08 22:47:10 -0700

    chore: Add script for extracting method refactoring instructions

---
+++ src/reinforcement-learning/playground/llama.py
@@ -0,0 +1,89 @@
+import torch
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer, DataCollatorForSeq2Seq
+from datetime import datetime
+
+train_dataset = load_dataset("json", data_files="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/src/reinforcement-learning/test-data/train.jsonl", split='train')
+eval_dataset = load_dataset("json", data_files="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/src/reinforcement-learning/test-data/val.jsonl", split='train')
+
+# print(dataset[1].keys())
+
+base_model = "codellama/CodeLlama-7b-hf"
+model = AutoModelForCausalLM.from_pretrained(
+    base_model,
+    load_in_8bit=True,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-7b-hf")
+
+tokenizer.add_eos_token = True
+tokenizer.pad_token_id = 0
+tokenizer.padding_side = "left"
+
+
+def tokenize(prompt):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=512,
+        padding=False,
+        return_tensors=None,
+    )
+
+    # "self-supervised learning" means the labels are also the inputs:
+    result["labels"] = result["input_ids"].copy()
+
+    return result
+
+def generate_and_tokenize_prompt(data_point):
+    full_prompt =f"""You are a powerful code refactoring model. Your job is to refactor the given method using extract method refactoring. 
+
+                    ### To be-refacored code:
+                    {data_point["Smelly Sample"]}
+
+                    ### Refactored Code:
+                    {data_point["Method after Refactoring"]}+"\n\n"+{data_point['Extracted Method']}
+                    """
+    return tokenize(full_prompt)
+
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
+tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt)
+
+model.train()
+batch_size = 128
+per_device_train_batch_size = 32
+gradient_accumulation_steps = batch_size // per_device_train_batch_size
+output_dir = "refactoring-output-model"
+
+training_args = TrainingArguments(
+        per_device_train_batch_size=per_device_train_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        warmup_steps=100,
+        max_steps=400,
+        learning_rate=3e-4,
+        fp16=True,
+        logging_steps=10,
+        optim="adamw_torch",
+        evaluation_strategy="steps", # if val_set_size > 0 else "no",
+        save_strategy="steps",
+        eval_steps=20,
+        save_steps=20,
+        output_dir=output_dir,
+        load_best_model_at_end=False,
+        group_by_length=True, # group sequences of roughly the same length together to speed up training
+        report_to="wandb", # if use_wandb else "none",
+        run_name=f"codellama-{datetime.now().strftime('%Y-%m-%d-%H-%M')}", # if use_wandb else None,
+    )
+
+trainer = Trainer(
+    model=model,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_val_dataset,
+    args=training_args,
+    data_collator=DataCollatorForSeq2Seq(
+        tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+    ),
+)
+
