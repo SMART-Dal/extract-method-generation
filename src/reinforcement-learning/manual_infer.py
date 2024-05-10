8264a779bb93cce0c463598002d199a0a6d10bd1
Indranil <indranil.palit@dal.ca>
2024-05-10 13:23:42 -0700

    feat: updated manual inference code

--- src/reinforcement-learning/manual_infer.py
+++ src/reinforcement-learning/manual_infer.py
@@ -3,25 +3,35 @@ import torch
 import pandas as pd
 from transformers import T5Config, T5ForConditionalGeneration, RobertaTokenizer
 
-tokenizer = RobertaTokenizer.from_pretrained("Salesforce/codet5-small")
-# model = T5ForConditionalGeneration.from_pretrained("/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/src/refactoring-finetune/CodeT5/sh/saved_models/refactoring/codet5_small_all_lr5_bs32_src320_trg256_pat5_e100/checkpoint-best-bleu")
-model = T5ForConditionalGeneration.from_pretrained("/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/src/reinforcement-learning/ppo-output")
+def data_gen(path):
+    with open(path, 'r') as f:
+        for line in f:
+            example = json.loads(line.strip())    
+            yield example
 
-model = model.to('cuda')
+def code_t5():
+    tokenizer = RobertaTokenizer.from_pretrained("Salesforce/codet5-small")
+    # model = T5ForConditionalGeneration.from_pretrained("/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/src/refactoring-finetune/CodeT5/sh/saved_models/refactoring/codet5_small_all_lr5_bs32_src320_trg256_pat5_e100/checkpoint-best-bleu")
+    model = T5ForConditionalGeneration.from_pretrained("/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/src/reinforcement-learning/ppo-output")
 
-ls_data = []
-with open("/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/src/reinforcement-learning/test-data/test.jsonl", 'r') as f:
-    for line in f:
-        example = json.loads(line.strip())
-        tok_input = tokenizer(example["Smelly Sample"].replace("\t"," "),return_tensors="pt", padding="max_length", max_length=512, truncation=True).input_ids
-        tok_input = tok_input.to('cuda')
-        # print(model.generate(tok_input))
-        # print(tok_input.size())
-        with torch.no_grad():
-            gen_tokens = model.generate(tok_input, max_length = 512)
+    model = model.to('cuda')
 
-        ft = tokenizer.decode(gen_tokens[0], skip_special_tokens=True)
-        ls_data.append([example["Smelly Sample"].replace("\t"," "),example["Method after Refactoring"].replace("\t"," ") + example["Extracted Method"].replace("\t"," "), ft])
+    ls_data = []
+    for example in data_gen():
+            tok_input = tokenizer(example["Smelly Sample"].replace("\t"," "),return_tensors="pt", padding="max_length", max_length=512, truncation=True).input_ids
+            tok_input = tok_input.to('cuda')
+            # print(model.generate(tok_input))
+            # print(tok_input.size())
+            with torch.no_grad():
+                gen_tokens = model.generate(tok_input, max_length = 512)
 
-df = pd.DataFrame(ls_data,columns=['ss','gt','ft'])
-df.to_csv('output_rl.csv', index=False)
+            ft = tokenizer.decode(gen_tokens[0], skip_special_tokens=True)
+            ls_data.append([example["Smelly Sample"].replace("\t"," "),example["Method after Refactoring"].replace("\t"," ") + example["Extracted Method"].replace("\t"," "), ft])
+
+    df = pd.DataFrame(ls_data,columns=['ss','gt','ft'])
+    df.to_csv('output_rl.csv', index=False)
+
+
+if __name__=="__main__":
+    for ex in data_gen("/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/src/reinforcement-learning/test-data/test.jsonl"):
+         print(ex) 
