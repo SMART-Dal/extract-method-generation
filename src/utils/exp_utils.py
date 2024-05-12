6d574b8c7222f356aa0dbf5f1b84b5b8fdac37f3
Indranil <indranil.palit@dal.ca>
2024-05-12 11:44:23 -0700

    refactor: Update exp_utils.py with improved calculation of average source and target lengths

--- src/utils/exp_utils.py
+++ src/utils/exp_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import json
+import sys
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from tqdm import tqdm
@@ -18,7 +19,7 @@ def generate_modified_data(dataset, tokenizer, file_name):
             sample = {"Input": str(data["Smelly Sample"]), "Output": str(data["Method after Refactoring"])+"\n"+str(data['Extracted Method'])}
             f.write(json.dumps(sample) + '\n')
 
-def calc_stats(examples, tokenizer=None):
+def calc_stats(examples, tokenizer=None, collation=False):
     avg_src_len = []
     avg_trg_len = []
     avg_trg_col_len = []
@@ -26,34 +27,40 @@ def calc_stats(examples, tokenizer=None):
     avg_trg_len_tokenize = []
     avg_trg_collated_len_tokenize = []
     src_count, trg_count, trg_col_count = 0,0,0
+    if collation:
+        keys = ("Smelly Sample", "Method after Refactoring", 'Extracted Method')
+    else:
+        keys = ("Input", "Output")   
     for ex in tqdm(examples):
 
-        avg_src_len.append(len(str(ex["Smelly Sample"]).split()))
-        avg_trg_len.append(len(str(ex["Method after Refactoring"]).split()))
-        avg_trg_col_len.append(len(str(ex["Method after Refactoring"]).split())+len(str(ex['Extracted Method']).split()))
+        avg_src_len.append(len(str(ex[keys[0]]).split()))
+        avg_trg_len.append(len(str(ex[keys[1]]).split()))
+        if collation:
+            avg_trg_col_len.append(len(str(ex[keys[1]]).split())+len(str(ex[keys[2]]).split()))
         
-        tmp = len(tokenizer.tokenize(ex["Smelly Sample"]))
+        tmp = len(tokenizer.tokenize(ex[keys[0]]))
         if tmp>512:
             src_count+=1
         avg_src_len_tokenize.append(tmp)
         
-        tmp = len(tokenizer.tokenize(str(ex["Method after Refactoring"])))
+        tmp = len(tokenizer.tokenize(str(ex[keys[1]])))
         if tmp>512:
             trg_count+=1
         avg_trg_len_tokenize.append(tmp)
         
-        tmp = len(tokenizer.tokenize(str(ex["Method after Refactoring"]+str(ex['Extracted Method']))))
-        if tmp>512:
-            trg_col_count+=1
-        avg_trg_collated_len_tokenize.append(tmp)
+        if collation:
+            tmp = len(tokenizer.tokenize(str(ex[keys[1]]+str(ex[keys[2]]))))
+            if tmp>512:
+                trg_col_count+=1
+            avg_trg_collated_len_tokenize.append(tmp)
 
         # avg_src_len.append(len(ex.source.split()))
         # avg_trg_len.append(len(str(ex.target).split()))
 
 
     print(f"Read {len(examples)} examples \navg src len: {np.mean(avg_src_len)}\n avg trg len: {np.mean(avg_trg_len)}\n avg trg col len: {np.mean(avg_trg_col_len)} \
-            \n max src len: {max(avg_src_len)}\n max trg len: {max(avg_trg_len)}\n max trg len col: {max(avg_trg_col_len)}")
-    print(f"[TOKENIZE] avg src len: {np.mean(avg_src_len_tokenize)}\n avg trg len: {np.mean(avg_trg_len_tokenize)}\n avg trg len col: {np.mean(avg_trg_collated_len_tokenize)}\n max src len: {max(avg_src_len_tokenize)}\n max trg len: {max(avg_trg_len_tokenize)}\n max trg len col: {max(avg_trg_collated_len_tokenize)}")
+            \n max src len: {max(avg_src_len)}\n max trg len: {max(avg_trg_len)}\n max trg len col: {max(avg_trg_col_len) if avg_trg_col_len else None }")
+    print(f"[TOKENIZE] avg src len: {np.mean(avg_src_len_tokenize)}\n avg trg len: {np.mean(avg_trg_len_tokenize)}\n avg trg len col: {np.mean(avg_trg_collated_len_tokenize) if avg_trg_collated_len_tokenize else None}\n max src len: {max(avg_src_len_tokenize)}\n max trg len: {max(avg_trg_len_tokenize)}\n max trg len col: {max(avg_trg_collated_len_tokenize) if avg_trg_collated_len_tokenize else None}")
     print(f"[TOKENIZE] src count above model max: {src_count} \n target count above model max: {trg_count}\n target col above model max: {trg_col_count}")
         # print("Read %d examples, avg src len: %d, avg trg len: %d, max src len: %d, max trg len: %d",
         #             len(examples), np.mean(avg_src_len), np.mean(avg_trg_len), max(avg_src_len), max(avg_trg_len))
@@ -99,25 +106,31 @@ def calc_stats_mod_method(examples, tokenizer=None):
 
 if __name__=="__main__":
     tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-small")
-    # print(tokenizer.__dict__)
     # # model = AutoModelForSeq2SeqLM.from_pretrained("Salesforce/codet5-small")
-    # calc_stats(load_dataset("json",
-    #                         data_files="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context/train.jsonl",
-    #                         split='train'),
-    #             tokenizer
-    #            )
-    generate_modified_data(load_dataset("json",
-                            data_files="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context/val.jsonl",
-                            split='train'),
-                            tokenizer,
-                            "val"
-                            )
-    # calc_stats_mod_method(load_dataset(
-    #     "json",
-    #     data_files="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context-len/val.jsonl",
-    #     split="train",
-    # ),
-    # tokenizer)
+    type = sys.argv[1]
+    if type == "calc":
+        data_files = sys.argv[2]
+        calc_stats(load_dataset("json",
+                                data_files=data_files,
+                                split='train'),
+                    tokenizer
+                )
+        # calc_stats_mod_method(load_dataset(
+        #     "json",
+        #     data_files="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context-len/val.jsonl",
+        #     split="train",
+        # ),
+        # tokenizer)        
+    elif type == "generate":
+        generate_modified_data(load_dataset("json",
+                                data_files="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context/val.jsonl",
+                                split='train'),
+                                tokenizer,
+                                "val"
+                                )
+    else:
+        print("Wrong Choice")        
+
 
     
     
