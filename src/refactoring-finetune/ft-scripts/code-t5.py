48455f202b5138f1a30102092fdca96162ec25e2
Indranil <indranil.palit@dal.ca>
2024-05-18 23:23:41 -0700

    refactor: Update code-t5.py script with new arguments and dataset paths

--- src/refactoring-finetune/ft-scripts/code-t5.py
+++ src/refactoring-finetune/ft-scripts/code-t5.py
@@ -1,26 +1,59 @@
 import torch
 import evaluate
 import numpy as np
+import os
 from datasets import load_dataset
 from transformers import (
     AutoTokenizer, 
     AutoModelForSeq2SeqLM, 
     DataCollatorForSeq2Seq, 
     Seq2SeqTrainer,
-    Seq2SeqTrainingArguments
+    Seq2SeqTrainingArguments,
+    HfArgumentParser
     )
 
+from dataclasses import dataclass, field
+from typing import Optional
 
-train_dataset = load_dataset("json",data_files="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context-len/train.jsonl",
-                    split="train")
+os.environ["WANDB_PROJECT"]="extract_method_refactoring_generation"
 
-eval_dataset = load_dataset("json",data_files="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context-len/val.jsonl",
-                    split="train")
+@dataclass
+class ScriptArguments:
+    """
+    The config class
+    """
+
+    model_name: Optional[str] = field(default="Salesforce/codet5-small", metadata={"help": "the model name or path"})
+    tokenizer_name: Optional[str] = field(default="Salesforce/codet5-small", metadata={"help": "the model name or path"})
+    log_with: Optional[str] = field(default=None, metadata={"help": "use 'wandb' to log with wandb"})
+    learning_rate: Optional[float] = field(default=(1.47e-5) * 2, metadata={"help": "the learning rate"})
+    batch_size: Optional[int] = field(default=8, metadata={"help": "the batch size"})
+    gradient_accumulation_steps: Optional[int] = field(
+        default=1, metadata={"help": "the number of gradient accumulation steps"}
+    )
+    model_save_path: Optional[str] = field(
+        default="./output/codet5-out",
+        metadata={"help": "the path to save the model"},
+    )
+    run_name: Optional[str] = field(default=None, metadata={"help":"the name of the experiment"})
+    train_data_file_path: Optional[str] = field(default=None,metadata={"help":"the path to the train data file"})
+    eval_data_file_path: Optional[str] = field(default=None,metadata={"help":"the path to the eval data file"})
+    test_data_file_path: Optional[str] = field(default=None,metadata={"help":"the path to the eval data file"})
+    num_epochs: Optional[int] = field(default=3,metadata={"help":"number of training epochs"})
+
+
+parser = HfArgumentParser(ScriptArguments)
+script_args = parser.parse_args_into_dataclasses()[0]
+
+
+train_dataset = load_dataset("json",data_files=script_args.train_data_file_path, split="train")
+
+eval_dataset = load_dataset("json",data_files=script_args.eval_data_file_path, split="train")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-small")
-model = AutoModelForSeq2SeqLM.from_pretrained("Salesforce/codet5-small")
+tokenizer = AutoTokenizer.from_pretrained(script_args.model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(script_args.model_name)
 
 def preprocess_function(examples):
     # for e in examples:
@@ -53,9 +86,6 @@ def preprocess_function(examples):
 train_dataset = train_dataset.map(
     preprocess_function,
     batched=True,
-    # num_proc=data_args.preprocessing_num_workers,
-    # remove_columns=column_names,
-    # load_from_cache_file=not data_args.overwrite_cache,
     desc="Running tokenizer on train dataset",
 )
 
@@ -65,10 +95,9 @@ eval_dataset = eval_dataset.map(
     desc="Running tokenizer on eval dataset"
 )
 
-metric = evaluate.load("sacrebleu", cache_dir="./cache_dir")
+metric1 = evaluate.load("sacrebleu", cache_dir="./cache_dir")
+metric2 = evaluate.load("rouge")
 
-# Data collator
-# label_pad_token_id = -100 if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id
 label_pad_token_id = -100
 data_collator = DataCollatorForSeq2Seq(
     tokenizer,
@@ -105,8 +134,12 @@ def compute_metrics(eval_preds):
     # Some simple post-processing
     decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
 
-    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
-    result = {"bleu": result["score"]}
+    result_bleu = metric1.compute(predictions=decoded_preds, references=decoded_labels)
+    result_bleu = {"bleu": result_bleu["score"]}
+
+    result_rouge = metric2.compute(predictions=decoded_preds, references=decoded_labels)
+
+    result = {**result_bleu, **result_rouge}
 
     prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
     result["gen_len"] = np.mean(prediction_lens)
@@ -114,11 +147,14 @@ def compute_metrics(eval_preds):
     return result
 
 training_args = Seq2SeqTrainingArguments(
-    output_dir="./output",
+    run_name=script_args.run_name,
+    output_dir=script_args.model_save_path,
+    overwrite_output_dir=True,
     predict_with_generate=True,
     evaluation_strategy="epoch",
-    num_train_epochs=5,
-    generation_max_length=512
+    num_train_epochs=script_args.num_epochs,
+    generation_max_length=512,
+    logging_steps=1
 )
 
 trainer = Seq2SeqTrainer(
@@ -145,3 +181,7 @@ metrics = train_result.metrics
 trainer.log_metrics("train", metrics)
 trainer.save_metrics("train", metrics)
 trainer.save_state()
+
+# python code-t5.py --model_save_path ./output/codet5-test --run_name code_t5_test --train_data_file_path /home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract
+# -method-generation/data/dl-no-context-len/train.jsonl --eval_data_file_path /home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-method-generation/data/dl-no-context-len/val.jsonl --num_epochs 
+# 1
