42e4318ffbede4e33ae40c0cf5831376b73f3a98
Indranil <indranil.palit@dal.ca>
2024-05-20 10:21:57 -0700

    feat: Update fine-tune.sh script with additional arguments for model and tokenizer names

--- fine-tune.sh
+++ fine-tune.sh
@@ -2,7 +2,7 @@
 #SBATCH --mem=64G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
-#SBATCH --time=6:0:0
+#SBATCH --time=12:0:0
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --signal=B:USR1@360
 #SBATCH --mail-user=indranil.palit@dal.ca
@@ -12,6 +12,8 @@ project_location="/home/ip1102/projects/def-tusharma/ip1102/Ref_RL/POC/extract-m
 
 RUN_NAME={$1}
 OUTPUT_FOLDER=${2}
+MODEL_NAME=${3}
+TOKENIZER_NAME=${4}
 
 cd $project_location
 
@@ -22,6 +24,8 @@ source .venv/bin/activate
 cd src/refactoring-finetune/ft-scripts/
 
 python supervised_fine_tune.py \
+--model_name $MODEL_NAME \
+--tokenizer_name $TOKENIZER_NAME \
 --model_save_path $OUTPUT_FOLDER \
 --run_name $RUN_NAME \
 --train_data_file_path $project_location/data/dl-no-context-len/train.jsonl \
